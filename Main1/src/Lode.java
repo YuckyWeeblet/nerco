e794469faaf4c120a19a4ba9a214a1cf7d1facd5
sykora4 <sykora4@PC507.SPSEJECNANET.CZ>
2021-06-11 11:52:54 +0200

    ok

--- Main1/src/Lode.java
+++ Main1/src/Lode.java
@@ -33,33 +33,223 @@ public class Lode {
 		switch(rotace) 
 		{
 		case 0:
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
 			
+			y++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x--;
+			y++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
 			break;
 		case 1:
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			y++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x--;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x++;
+			y++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
 			
 			break;
 		case 2:
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			y++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			y--;
+			x++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
 			
 			break;
 		case 3:
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			x++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			y--;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
+			
+			y++;
+			x++;
+			
+			for(int i = 0; i<idk.size(); i++) 
+			{
+				if(idk.get(i).x == x && idk.get(i).y == y) 
+				{
+					if(idk.get(i).getShip() == true)
+					{
+						j = true;
+					}
+				}		
+			}
 			
 			break;
 		
 		}
 		
-	for() 
-	{
-		for(int i = 0; i<idk.size(); i++) 
-		{
-			if(idk.get(i).x == x && idk.get(i).y == y) 
-			{
-				if(idk.get(i).getShip() == true)
-				{
-					j = true;
-				}
-			}
-		}
-	}
+	
+	
+		
+	
 	
 	
 	
